86f6a758188e1e1277bc75e2b0babda0a5278838
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2023-09-17 11:59:48 -0400

    Create learnPython.py

---
+++ learnPython.py
@@ -0,0 +1,64 @@
+import random #this is needed for later things
+
+#ITS TIME TO LEARN, FINISH ALL THE FUNCTIONS (call them at the bottom)
+
+#create a function that prints a word
+def main0():
+  print()
+
+#create a function that prints 2 numbers added together
+def main1():
+  num0 = None
+  num1 = None
+  num3 = null + null
+  print(num3)
+
+#create a function that adds 2 random numbers and prints the result
+def main2():
+  num0 = random.randint(0,200)
+  pass #remove this line
+
+#create a function that adds to user inputted numbers
+def main3():
+  num0 = input()
+  pass
+
+#create a function that counts to a user inputted number plus a random number then prints "Done" followed by the random number that was added
+def main4():
+  while null != 0: 
+    pass
+  pass
+
+#create a function that calls all of the previos functions in a row
+def main5():
+  pass
+
+#create a function that tests to user inputted numbers to see if they are the same, the same as a third number, greater than another and so on.
+def main6():
+  if null == null:
+    pass
+  elif null == null:
+    pass
+  pass 
+# != means does not equal
+# == means does equal
+# <= means less than or equal to
+# < means less than
+# and, or can be used to do two conditionals at once
+
+#create a function that allows the user to choose and run any of the othee functions
+def main7():
+  pass
+
+
+
+#you can call your function down here. Simply erase the # of the one you want to call add put it back on the ones you don't want to call
+
+main0()
+#main1()
+#main2()
+#main3()
+#main4()
+#main5()
+#main6()
+#main7()
